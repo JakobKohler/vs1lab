3cf6212fb727b4edb1bd8a12dd47a93e303d0005
Jakob Kohler <schasch.dev@protonmail.com>
2023-04-12 15:14:36 +0200

    PENG

--- Aufgabe2/gta_v2/public/javascripts/effects.js
+++ Aufgabe2/gta_v2/public/javascripts/effects.js
@@ -1,13 +1,17 @@
 const hashtag_input = document.getElementById('hashtag');
 const sinz = document.getElementById('sinz');
+const map = document.getElementById('map_img');
 const footer = document.getElementById('footer');
 
 hashtag_input.addEventListener('input', (value) => {
     if(hashtag_input.value.includes('sinz')){
         sinz.style.transform = 'translateY(-100%)';
         footer.style.overflow = 'visible';
+    }else if(hashtag_input.value.includes('peng')){
+        map.style.setProperty('--peng-transform', 'translateX(-100px) scale(0.25) rotate(-30deg)');
     }else{
         sinz.style.transform = 'translateY(0)';
+        map.style.setProperty('--peng-transform', 'translateX(0) scale(0.25) rotate(0)');
     }
 });
 
