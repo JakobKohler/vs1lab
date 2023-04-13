b4900fe77c21a56f2602add3803dacfff0136d85
Jakob Kohler <schasch.dev@protonmail.com>
2023-04-13 10:04:42 +0200

    Fixed feautures

--- Aufgabe2/gta_v2/public/javascripts/effects.js
+++ Aufgabe2/gta_v2/public/javascripts/effects.js
@@ -1,22 +1,14 @@
 const hashtag_input = document.getElementById('hashtag');
-const sinz = document.getElementById('sinz');
 const map = document.getElementById('map_img');
 const footer = document.getElementById('footer');
 
 hashtag_input.addEventListener('input', (value) => {
     if(hashtag_input.value.includes('sinz')){
-        sinz.style.transform = 'translateY(-100%)';
-        footer.style.overflow = 'visible';
+        footer.style.setProperty('--sinz-scout', 'scale(0.3) translateY(-280px)')
     }else if(hashtag_input.value.includes('peng')){
         map.style.setProperty('--peng-transform', 'translateX(-100px) scale(0.25) rotate(-30deg)');
     }else{
-        sinz.style.transform = 'translateY(0)';
         map.style.setProperty('--peng-transform', 'translateX(0) scale(0.25) rotate(0)');
-    }
-});
-
-sinz.addEventListener('transitionend', () => {
-    if(!hashtag_input.value.includes('sinz')){
-        footer.style.overflow = 'hidden';
+        footer.style.setProperty('--sinz-scout', 'scale(0.3) translateY(0px)')
     }
 });
