231bf96a3880498e0cd58683894db03e56c44939
Jakob Kohler <schasch.dev@protonmail.com>
2023-04-25 14:39:20 +0200

    Some small fixes

--- Aufgabe3/gta_v3/routes/index.js
+++ Aufgabe3/gta_v3/routes/index.js
@@ -10,26 +10,26 @@
  * Define module dependencies.
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /**
- * The module "geotag" exports a class GeoTagStore. 
+ * The module "geotag" exports a class GeoTagStore.
  * It represents geotags.
- * 
+ *
  * TODO: implement the module in the file "../models/geotag.js"
  */
 // eslint-disable-next-line no-unused-vars
-const GeoTag = require('../models/geotag');
+const GeoTag = require("../models/geotag");
 
 /**
- * The module "geotag-store" exports a class GeoTagStore. 
+ * The module "geotag-store" exports a class GeoTagStore.
  * It provides an in-memory store for geotag objects.
- * 
+ *
  * TODO: implement the module in the file "../models/geotag-store.js"
  */
 // eslint-disable-next-line no-unused-vars
-const GeoTagStore = require('../models/geotag-store');
+const GeoTagStore = require("../models/geotag-store");
 
 const currentStore = new GeoTagStore();
 
@@ -43,9 +43,9 @@ const currentStore = new GeoTagStore();
  */
 
 // TODO: extend the following route example if necessary
-router.get('/', (req, res) => {
-  console.log('SIIIUUU');
-  res.render('index', { taglist: []})
+router.get("/", (req, res) => {
+  console.log("SIIIUUU");
+  res.render("index", { taglist: [] });
 });
 
 /**
@@ -59,16 +59,24 @@ router.get('/', (req, res) => {
  *
  * As response, the ejs-template is rendered with geotag objects.
  * All result objects are located in the proximity of the new geotag.
- * To this end, "GeoTagStore" provides a method to search geotags 
+ * To this end, "GeoTagStore" provides a method to search geotags
  * by radius around a given location.
  */
 
-router.post('/tagging', (req, res) => {
-    const newTag = [req.body.text_field_name, req.body.text_field_latitude, req.body.text_field_longitude, req.body.text_field_hashtag];
-    currentStore.addGeoTag(newTag);
+router.post("/tagging", (req, res) => {
+  const newTag = [
+    req.body.text_field_name,
+    req.body.text_field_latitude,
+    req.body.text_field_longitude,
+    req.body.text_field_hashtag,
+  ];
+  currentStore.addGeoTag(newTag);
 
-    let nearbyTags = currentStore.getNearbyGeoTags(req.body.text_field_latitude, req.body.text_field_longitude);
-    res.render('index', { taglist: nearbyTags });
+  let nearbyTags = currentStore.getNearbyGeoTags(
+    req.body.text_field_latitude,
+    req.body.text_field_longitude
+  );
+  res.render("index", { taglist: nearbyTags });
 });
 
 /**
@@ -81,35 +89,39 @@ router.post('/tagging', (req, res) => {
  *
  * As response, the ejs-template is rendered with geotag objects.
  * All result objects are located in the proximity of the given coordinates.
- * If a search term is given, the results are further filtered to contain 
- * the term as a part of their names or hashtags. 
- * To this end, "GeoTagStore" provides methods to search geotags 
+ * If a search term is given, the results are further filtered to contain
+ * the term as a part of their names or hashtags.
+ * To this end, "GeoTagStore" provides methods to search geotags
  * by radius and keyword.
  */
 
-router.post('/discovery', (req, res) => {
-  let nearbyTags = currentStore.searchNearbyGeoTags(req.body.text_field_search_lat, req.body.text_field_search_lon, req.body.text_field_searchterm);
-  res.render('index', { taglist: nearbyTags });
-})
+router.post("/discovery", (req, res) => {
+  let nearbyTags = currentStore.searchNearbyGeoTags(
+    req.body.text_field_search_lat,
+    req.body.text_field_search_lon,
+    req.body.text_field_searchterm
+  );
+  res.render("index", { taglist: nearbyTags });
+});
 
 /* about */
-router.get('/about', (req,res) =>{
-  res.render('about');
-})
+router.get("/about", (req, res) => {
+  res.render("about");
+});
 
 /* help */
-router.get('/help', (req,res) =>{
-  res.render('help');
-})
+router.get("/help", (req, res) => {
+  res.render("help");
+});
 
 /* imprint */
-router.get('/imprint', (req,res) =>{
-  res.render('imprint');
-})
+router.get("/imprint", (req, res) => {
+  res.render("imprint");
+});
 
 /* privacy */
-router.get('/privacy', (req,res) =>{
-  res.render('privacy');
-})
+router.get("/privacy", (req, res) => {
+  res.render("privacy");
+});
 
 module.exports = router;
